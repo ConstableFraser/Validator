e561184e68355ea0b0b20e432bd131c0480338c1
snswd <sanswed@gmail.com>
2024-02-04 01:29:38 +0300

    [step#5] added shape method for MapSchema + refactoring

--- app/src/main/java/hexlet/code/Shemas/MapSchema.java
+++ app/src/main/java/hexlet/code/Shemas/MapSchema.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.function.Predicate;
 
 public class MapSchema extends BaseSchema<Map<String, String>> {
-    private final Map<String, Predicate<Map<?, ?>>> listChecksMap;
+    private final Map<String, Predicate<Map<String, String>>> listChecksMap;
     private Map<String, BaseSchema<String>> listChecksValues;
 
     public MapSchema() {
